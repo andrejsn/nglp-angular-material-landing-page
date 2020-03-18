363c77d57773a5b05155c4770293a0647bd0ef1e
andrejsn <andrejs@naumovs.de>
2020-03-19 00:47:42 +0100

    added circle animation

--- src/app/ranges/ranges.component.ts
+++ src/app/ranges/ranges.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-ranges',
-  templateUrl: './ranges.component.html',
-  styleUrls: ['./ranges.component.css']
+  selector: "app-ranges",
+  templateUrl: "./ranges.component.html",
+  styleUrls: ["./ranges.component.css"]
 })
 export class RangesComponent implements OnInit {
+  support = 0;
+  support_ = 20;
 
-  constructor() { }
+  testing = 0;
+  testing_ = 45;
+
+  development = 0;
+  development_ = 35;
+
+  constructor() {}
 
   ngOnInit() {
+    (async () => {
+      do {
+        this.support++;
+        this.testing++;
+        this.development++;
+        await this.delay(50);
+      } while (this.support < this.support_);
+
+      do {
+        this.testing++;
+        this.development++;
+        await this.delay(50);
+      } while (this.development < this.development_);
+
+      do {
+        this.testing++;
+        await this.delay(50);
+      } while (this.testing < this.testing_);
+    })();
   }
 
+  delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 }

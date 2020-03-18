9beaf243e57139159edd97c8cbf3672ae308050d
andrejsn <andrejs@naumovs.de>
2020-03-19 00:27:01 +0100

    added circle ranges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { DirectivesModule } from './directives/directives.module';
-import { Material2Module } from './material2.module';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { DirectivesModule } from "./directives/directives.module";
+import { Material2Module } from "./material2.module";
+import { RoundProgressModule } from "angular-svg-round-progressbar";
 
-
-import { AppComponent } from './app.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { AboutComponent } from './about/about.component';
-import { HeadingComponent } from './heading/heading.component';
-import { PricingComponent } from './pricing/pricing.component';
-import { BlogComponent } from './blog/blog.component';
-import { ContactComponent } from './contact/contact.component';
-import { ContactDialogComponent } from './contact-dialog/contact-dialog.component';
-import { WorkingComponent } from './working/working.component';
-import { RangesComponent } from './ranges/ranges.component';
-
-
+import { AppComponent } from "./app.component";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
+import { AboutComponent } from "./about/about.component";
+import { HeadingComponent } from "./heading/heading.component";
+import { PricingComponent } from "./pricing/pricing.component";
+import { BlogComponent } from "./blog/blog.component";
+import { ContactComponent } from "./contact/contact.component";
+import { ContactDialogComponent } from "./contact-dialog/contact-dialog.component";
+import { WorkingComponent } from "./working/working.component";
+import { RangesComponent } from "./ranges/ranges.component";
 
 @NgModule({
   declarations: [
@@ -40,10 +38,11 @@ import { RangesComponent } from './ranges/ranges.component';
     FlexLayoutModule,
     DirectivesModule,
     FormsModule,
-    ReactiveFormsModule,    
+    ReactiveFormsModule,
+    RoundProgressModule,
   ],
   providers: [],
-  entryComponents: [ ContactDialogComponent ],
+  entryComponents: [ContactDialogComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

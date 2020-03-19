93a34eb3c16c2e1103e9b24e5aa4cc6e02e2777a
andrejsn <andrejs@naumovs.de>
2020-03-19 09:48:43 +0100

    added pricing components

--- src/app/pricing/pricing.component.ts
+++ src/app/pricing/pricing.component.ts
@@ -1,38 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
-export interface Specification {
-  item: string;
-  cost: number;
-}
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-pricing',
-  templateUrl: './pricing.component.html',
-  styleUrls: ['./pricing.component.css']
+  selector: "app-pricing",
+  templateUrl: "./pricing.component.html",
+  styleUrls: ["./pricing.component.css"]
 })
 export class PricingComponent implements OnInit {
+  constructor() {}
 
-  isHeading = true;
-
-  displayedColumns: string[] = ['item', 'cost'];
-
-  specifications: Specification[] = [
-    {item: '1000mm Base unit', cost: 150},
-    {item: '1000mm Base unit', cost: 130},
-    {item: '600mm Built under', cost: 200},
-    {item: '600mm Larder unit', cost: 300},
-    {item: '500mm Wall unit', cost: 250},
-    {item: '500mm Wall unit', cost: 135},
-    {item: '1000mm Wall unit', cost: 205},
-  ];
-
-  constructor() { }
-
-  getTotalCost() {
-    return this.specifications.map(t => t.cost).reduce((acc, value) => acc + value, 0);
-  }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }

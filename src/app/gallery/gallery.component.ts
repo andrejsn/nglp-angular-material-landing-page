f1a60d7d133531a18004f4594914594c4232dc60
andrejsn <andrejs@naumovs.de>
2020-03-20 02:45:45 +0100

    added fontawesome

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
 
 @Component({
   selector: 'app-gallery',
@@ -6,10 +7,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent implements OnInit {
+  galleryOptions: NgxGalleryOptions[];
+    galleryImages: NgxGalleryImage[];
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit(): void {
+
+    this.galleryOptions = [
+        {
+            width: '600px',
+            height: '400px',
+            thumbnailsColumns: 4,
+            imageAnimation: NgxGalleryAnimation.Slide
+        },
+        // max-width 800
+        {
+            breakpoint: 800,
+            width: '100%',
+            height: '600px',
+            imagePercent: 80,
+            thumbnailsPercent: 20,
+            thumbnailsMargin: 20,
+            thumbnailMargin: 20
+        },
+        // max-width 400
+        {
+            breakpoint: 400,
+            preview: false
+        }
+    ];
+
+    this.galleryImages = [
+        {
+            small: 'assets/gallery/background4.jpg',
+            medium: 'assets/gallery/background4.jpg',
+            big: 'assets/gallery/background4.jpg'
+        },
+        {
+            small: 'assets/gallery/background4.jpg',
+            medium: 'assets/gallery/background4.jpg',
+            big: 'assets/gallery/background4.jpg'
+        },
+        {
+            small: 'assets/gallery/background4.jpg',
+            medium: 'assets/gallery/background4.jpg',
+            big: 'assets/gallery/background4.jpg'
+        }
+    ];
+}
 
 }

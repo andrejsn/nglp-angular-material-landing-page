7dc6f55440c88373abf20d34d181b0d16aa66e20
andrejsn <andrejs@naumovs.de>
2020-03-20 13:09:23 +0100

    init minory chang

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
+
 
 @Component({
   selector: 'app-gallery',
@@ -7,54 +7,11 @@ import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gal
   styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent implements OnInit {
-  galleryOptions: NgxGalleryOptions[];
-    galleryImages: NgxGalleryImage[];
+
 
   constructor() { }
 
   ngOnInit(): void {
-
-    this.galleryOptions = [
-        {
-            width: '600px',
-            height: '400px',
-            thumbnailsColumns: 4,
-            imageAnimation: NgxGalleryAnimation.Slide
-        },
-        // max-width 800
-        {
-            breakpoint: 800,
-            width: '100%',
-            height: '600px',
-            imagePercent: 80,
-            thumbnailsPercent: 20,
-            thumbnailsMargin: 20,
-            thumbnailMargin: 20
-        },
-        // max-width 400
-        {
-            breakpoint: 400,
-            preview: false
-        }
-    ];
-
-    this.galleryImages = [
-        {
-            small: 'assets/gallery/background4.jpg',
-            medium: 'assets/gallery/background4.jpg',
-            big: 'assets/gallery/background4.jpg'
-        },
-        {
-            small: 'assets/gallery/background4.jpg',
-            medium: 'assets/gallery/background4.jpg',
-            big: 'assets/gallery/background4.jpg'
-        },
-        {
-            small: 'assets/gallery/background4.jpg',
-            medium: 'assets/gallery/background4.jpg',
-            big: 'assets/gallery/background4.jpg'
-        }
-    ];
 }
 
 }

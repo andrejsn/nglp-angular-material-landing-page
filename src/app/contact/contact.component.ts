1e1b9fdca02c63d1474979a87cc5d6c895b3b475
andrejsn <andrejs@naumovs.de>
2020-04-20 21:03:15 +0200

    added no robot check box

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormGroupDirective, NgForm, FormControl, Validators } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 import { Contact } from './contact';
+import { SnotifyService } from 'ng-snotify';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class NgLpErrorStateMatcher implements ErrorStateMatcher {
@@ -17,33 +19,76 @@ export class NgLpErrorStateMatcher implements ErrorStateMatcher {
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  @ViewChild("contactForm") contactForm: NgForm;
 
-  nameFormControl = new FormControl('', [
-    Validators.required
-  ]);
+  isRobot = true;
+  isSubmitDisabled = false;
 
-  phoneFormControl = new FormControl('', [
-    Validators.required
-  ]);
-
-  emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
+  contactFormGroup = new FormGroup(
+    {
+      nameFormControl: new FormControl('', [
+        Validators.required
+      ]),
+      phoneFormControl: new FormControl('', [
+        Validators.required
+      ]),
+      emailFormControl: new FormControl('', [
+        Validators.required,
+        Validators.email,
+      ]),
+      messageFormControl: new FormControl('', []),
+      robotFormControl: new FormControl()
+    }
+  );
 
   matcher = new NgLpErrorStateMatcher();
 
   contact = new Contact();
 
 
-  constructor() { }
+  constructor(private snotifyService: SnotifyService) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   onSubmit() {
 
-  	console.log("FORM SEND");
+    console.log(this.contactFormGroup.get('robotFormControl').value);
 
+
+
+    if (this.contactFormGroup.get('nameFormControl').errors ||
+      this.contactFormGroup.get('phoneFormControl').errors ||
+      this.contactFormGroup.get('emailFormControl').errors) {
+      this.snotifyService.error('ERROR !');
+      return;
+    }
+
+
+    if (this.isRobot) {
+      this.isRobot = false;
+      this.isSubmitDisabled = true;
+      return;
+    }
+
+
+    console.log("FORM SEND");
+
+    this.snotifyService.info('OK');
+
+    this.reset();
+  }
+
+  setSubmitButton(event: MatCheckboxChange) {
+    this.isSubmitDisabled = !event.checked;
+  }
+
+  private reset() {
+    // reset robot
+    this.isRobot = true;
+
+    // reset form
+    this.contactFormGroup.reset();
+    this.contactForm.resetForm();
   }
 
 }
